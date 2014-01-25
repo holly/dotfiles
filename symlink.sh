261021e0d479623548ec8ed4ab5e61d0750cd00c
holly <emperor.kurt@gmail.com>
2014-01-25 19:06:10 +0900

    update distributin bashrc

--- symlink.sh
+++ symlink.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 cd $(dirname $0)
-DOTFILES=$(ls -A | grep -e "^\." | grep -ve "^\.git$" | grep -ve "^\.gitmodules$" | grep -ve "^\.gitignore")
+
+REDHAT_RELEASE=/etc/redhat-release
+OS_RELEASE=/etc/os-release
+if [ -e $REDHAT_RELEASE ]; then
+	OS=$(awk '{ print $1 }' $REDHAT_RELEASE | tr '[:upper:]' '[:lower:]')
+elif [ -e $OS_RELEASE ]; then
+	OS=$(grep -e '^ID=' $OS_RELEASE | sed -e 's/ID=//')
+else
+	OS=unknown
+fi
+ln -sf ".bashrc.$OS" .bashrc
+
+DOTFILES=$(ls -A | grep -e "^\." | grep -ve "^\.git$" | grep -ve "^\.gitmodules$" | grep -ve "^\.gitignore" | grep -ve '^\.bashrc\.')
 DATE=$(date +%y%m%d)
 
+
 if [ -r ~/.bash_profile -a ! -L ~/.bash_profile ]; then
-	mv ~/.bash_profile ~/.bash_profile.$DATE	
+	mv ~/.bash_profile ~/.bash_profile.$DATE
 fi
 
 if [ ! -r ~/.bash_aliases ]; then
