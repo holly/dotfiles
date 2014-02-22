c2f0401a69f905d305da56b41bee6959a187b579
holly <emperor.kurt@gmail.com>
2014-02-22 22:58:45 +0900

    update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -12,7 +12,13 @@ if [ -d $REAL_DOTSSH ]; then
 fi
 
 mkdir $REAL_DOTSSH
+# make authorized_keys
 find $DOTSSH/authorized_keys.d/ -type f ! -name .gitignore | xargs --no-run-if-empty cat >$REAL_DOTSSH/authorized_keys
+
+# make config
 find $DOTSSH/config.d/ -type f ! -name .gitignore ! -name base | xargs cat $DOTSSH/config.d/base >$REAL_DOTSSH/config
+
+# copy other files
+find $DOTSSH/extra/ -type f ! -name .gitignore | xargs -n1 -I% --run-if-empty cp -p % $REAL_DOTSSH/
 chmod 700 $REAL_DOTSSH
 chmod 600 $REAL_DOTSSH/authorized_keys $REAL_DOTSSH/config
