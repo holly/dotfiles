bf89ee6359ab6a2a8e302730d272d439b9e7acf6
holly <webmaster@holly9.uk>
2023-05-14 16:37:41 +0900

    from awk to cut command

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ set -o pipefail
 cd $(dirname $0)
 
 local_hash=$(git log -1 --pretty=%H HEAD)
-remote_hash=$(git ls-remote origin HEAD | awk '{ print $0 }')
+remote_hash=$(git ls-remote origin HEAD | cut -f1)
 
 if [[ "$local_hash" == "$remote_hash" ]]; then
     echo "local and remote branch are syncronized."
