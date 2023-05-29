f4b4df349828684971c1082dd08d4401cc7ad6c6
holly <webmaster@holly9.uk>
2023-05-29 18:19:59 +0900

    add has_bom

--- .config/fish/conf.d/util.fish
+++ .config/fish/conf.d/util.fish
@@ -2,6 +2,12 @@
 # utility functions
 ###################################
 
+function error
+
+    set_color --bold red; echo "[ERROR]: " $argv
+    return 1
+end
+
 function is_executable_sudo
 
     timeout --signal=2 1 sudo -l >/dev/null 2>&1
@@ -13,6 +19,29 @@ function is_root
         return 0
     end
     return 1
+end
+
+function is_stdin
+
+    if test -t 0
+        return 1
+    end
+    return 0
+end
+
+function has_bom
+
+    if test (count $argv) -eq 0
+        error "argv is required"
+        return 1
+    end
+    set -l f $argv[1]
+    if not test -f $f
+        error "argv[1]($f) is not file"
+        return $status
+    end
+    string match -ar '\(with BOM\)' (file $f) >/dev/null
+    return $status
 end
 
 function fup
