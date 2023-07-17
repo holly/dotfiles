415b24a178f624daff140ae48cba7421c4bb7f10
holly <webmaster@holly9.uk>
2023-07-17 12:13:50 +0900

    more update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,10 +25,7 @@ set -gx DOCKER_BUILDKIT 1
 ###################################
 # abbr
 ###################################
-abbr -a vi nvim
-abbr -a view "nvim -R"
-abbr -a vimrc "nvim $HOME/.config/nvim/init.vim"
-
+# for aws cli
 if command -sq aws
     abbr -a s3  "aws s3"
     abbr -a ec2 "aws ec2"
@@ -77,25 +74,6 @@ if command -sq docker
     abbr -a drmi "docker images | grep -v REPO | awk '{ print \$3 }' | xargs docker rmi "
 end
 
-
-# for common
-abbr -a x "chmod +x"
-abbr -a strace "strace -s 1024 -tt -f -T -v"
-abbr -a od "od -tcx1"
-abbr -a tcf "tar --exclude-backups --exclude=\"*.old\" --exclude=\"*.swp\" --exclude=\"*.tmp\" --exclude=\"*.org\" --exclude=\"*.pid\" --exclude=\"lock\" --exclude=\"*~\" --ignore-failed-read --one-file-system -pcvzf"
-abbr -a txf "tar --ignore-failed-read -pxvf"
-abbr -a tat "tar --ignore-failed-read -ptvf"
-abbr -a epoch "date +%s"
-abbr -a dt "date --iso-8601=seconds"
-abbr -a df "duf"
-abbr -a comma "printf \"%'d\n\""
-abbr -a delempty "egrep -v '^\$'"
-abbr -a alignlf "perl -i -nlpe 's/\x0d//g'"
-
-# for web service
-abbr -a myip "curl -s https://ipinfo.io/ip; echo"
-abbr -a ipinfo "curl -s https://ipinfo.io/"
-
 # for git 
 abbr -a g  "git"
 abbr -a ga "git add . && git add -u && git status"
@@ -117,4 +95,26 @@ abbr -a post "curl -sfSL -XPOST"
 abbr -a put "curl -sfSL -XPUT"
 abbr -a cw "curl -sfSL -o /dev/null -w '%{json}'"
 
+# for nvim
+abbr -a vi nvim
+abbr -a view "nvim -R"
+abbr -a vimrc "nvim $HOME/.config/nvim/init.vim"
 
+
+# for web service
+abbr -a myip "curl -s https://ipinfo.io/ip; echo"
+abbr -a ipinfo "curl -s https://ipinfo.io/"
+
+# for common
+abbr -a x "chmod +x"
+abbr -a strace "strace -s 1024 -tt -f -T -v"
+abbr -a od "od -tcx1"
+abbr -a tcf "tar --exclude-backups --exclude=\"*.old\" --exclude=\"*.swp\" --exclude=\"*.tmp\" --exclude=\"*.org\" --exclude=\"*.pid\" --exclude=\"lock\" --exclude=\"*~\" --ignore-failed-read --one-file-system -pcvzf"
+abbr -a txf "tar --ignore-failed-read -pxvf"
+abbr -a tat "tar --ignore-failed-read -ptvf"
+abbr -a epoch "date +%s"
+abbr -a dt "date --iso-8601=seconds"
+abbr -a df "duf"
+abbr -a comma "printf \"%'d\n\""
+abbr -a delempty "egrep -v '^\$'"
+abbr -a alignlf "perl -i -nlpe 's/\x0d//g'"
