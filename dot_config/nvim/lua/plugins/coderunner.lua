c7496df8cf774946f27e19e8b52d5c8dbd14ca68
holly <info@holly9.uk>
2026-04-19 21:50:30 +0900

    feat: ✨ Add Perl and Bash support to code runner config

--- dot_config/nvim/lua/plugins/coderunner.lua
+++ dot_config/nvim/lua/plugins/coderunner.lua
@@ -1,44 +1,47 @@
 return {
-  'CRAG666/code_runner.nvim',
-  keys = {
-    { '<leader>r', '<cmd>RunCode<cr>', desc = 'Run Code' },
-  },
-  config = function()
-    local function find_root(markers)
-      local path = vim.fn.expand('%:p:h')
-      while path ~= '/' do
-        for _, marker in ipairs(markers) do
-          if vim.fn.filereadable(path .. '/' .. marker) == 1 or
-             vim.fn.isdirectory(path .. '/' .. marker) == 1 then
-            return path
-          end
+    'CRAG666/code_runner.nvim',
+    keys = {
+        { '<leader>r', '<cmd>RunCode<cr>', desc = 'Run Code' },
+    },
+    config = function()
+        local function find_root(markers)
+            local path = vim.fn.expand('%:p:h')
+            while path ~= '/' do
+                for _, marker in ipairs(markers) do
+                    if vim.fn.filereadable(path .. '/' .. marker) == 1 or
+                        vim.fn.isdirectory(path .. '/' .. marker) == 1 then
+                        return path
+                    end
+                end
+                path = vim.fn.fnamemodify(path, ':h')
+            end
+            return nil
         end
-        path = vim.fn.fnamemodify(path, ':h')
-      end
-      return nil
-    end
 
-    require('code_runner').setup({
-      filetype = {
-        typescript = "bun run",
-        javascript = "bun run",
-        python = function()
-          if find_root({'uv.lock'}) then
-            return 'uv run'
-          elseif find_root({'poetry.lock'}) then
-            return 'poetry run python'
-          elseif find_root({'Pipfile'}) then
-            return 'pipenv run python'
-          else
-            return 'python -u'
-          end
-        end,
-        markdown = "glow",
-      },
+        require('code_runner').setup({
+            filetype = {
+                typescript = "bun run",
+                javascript = "bun run",
+                python     = function()
+                    if find_root({ 'uv.lock' }) then
+                        return 'uv run'
+                    elseif find_root({ 'poetry.lock' }) then
+                        return 'poetry run python'
+                    elseif find_root({ 'Pipfile' }) then
+                        return 'pipenv run python'
+                    else
+                        return 'python -u'
+                    end
+                end,
+                markdown   = "glow",
+                perl       = "perl",
+                sh         = "bash",
+                bash       = "bash",
+            },
 
-      before_run_filetype = function()
-        vim.cmd('write')
-      end,
-    })
-  end,
+            before_run_filetype = function()
+                vim.cmd('write')
+            end,
+        })
+    end,
 }
